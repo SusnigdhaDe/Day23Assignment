07250739ad09cff4bcb1535b8e7e3d3c87df77d9
Susnigdha De <susnigdha.de@gmail.com>
2020-11-15 21:31:09 +0530

    Start with Displaying Welcome to Employee Wage Computation Program on Master Branch

---
+++ Welcome.java
@@ -0,0 +1,10 @@
+public class Welcome {
+
+	public static void main(String[] args) {
+
+	//Start with Displaying Welcome to Employee Wage Computation
+	//Program on Master Branch
+
+	System.out.println("Welcome");
+	}
+}
